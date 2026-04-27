d3f0b9bf80e830cf50447284d8f9f7341994a4af
sizemosd-cloud <sizemosd@miamioh.edu>
2026-04-27 11:41:08 -0400

    Update 01_data_acquisition.R

--- 01_data_acquisition.R
+++ 01_data_acquisition.R
@@ -10,16 +10,10 @@ library(robotstxt)
 
 # URL 1
 
-url1<- "https://www.baseball-reference.com/?__hstc=213859787.89132add1a213af5a4f4159557c447b0.1776445715593.1776445715593.1776445715593.1&__hssc=213859787.2.1776445715593&__hsfp=ef6c28a7383133179055c075b37a64fd"
-paths_allowed(paths ="https://www.baseball-reference.com/?__hstc=213859787.89132add1a213af5a4f4159557c447b0.1776445715593.1776445715593.1776445715593.1&__hssc=213859787.2.1776445715593&__hsfp=ef6c28a7383133179055c075b37a64fd" )
-
-
-# URL 2
-
-url2<- "https://www.spotrac.com/mlb/rankings/player/_/year/2025/sort/cap_total"
+url1<- "https://www.spotrac.com/mlb/rankings/player/_/year/2025/sort/cap_total"
 paths_allowed(paths="https://www.spotrac.com/mlb/rankings/player/_/year/2025/sort/cap_total")
 
-page2 <- read_html(GET(url2, add_headers(`User-Agent` = "Mozilla/5.0")))
+page2 <- read_html(GET(url1, add_headers(`User-Agent` = "Mozilla/5.0")))
 
 # NOW you can scrape the elements
 player_names <- page2 |> 
@@ -34,6 +28,7 @@ head(player_names)
 head(player_salaries)
 
 # Scrape positions
+
 
 
 
