b8c89519a4805999bc4363ac59633e8335e921a2
paigecharlton <prcharlton7@gmail.com>
2026-04-29 10:35:44 -0400

    Update 01_data_acquisition.R

--- scripts/01_data_acquisition.R
+++ scripts/01_data_acquisition.R
@@ -57,3 +57,53 @@ all_players_2025 <- bind_rows(hitter_data, pitcher_data) %>%
   distinct(player_full_name, .keep_all = TRUE)
 
 head(all_players_2025)
+
+
+# WAR 
+
+library(httr)
+library(jsonlite)
+library(dplyr)
+
+base_url <- "https://www.fangraphs.com/api/leaders/war"
+
+get_page <- function(page_num) {
+  res <- GET(base_url, query = list(
+    season = 2025,
+    team = 19,
+    wartype = 1,
+    position = "all",
+    league = "all",
+    page = page_num,
+    pageSize = 100
+  ))
+  
+  data <- fromJSON(content(res, "text", encoding = "UTF-8"))
+  return(data$data)
+}
+
+# figure out total pages
+first <- GET(base_url, query = list(
+  season = 2025,
+  team = 19,
+  wartype = 1,
+  position = "all",
+  league = "all",
+  page = 1,
+  pageSize = 100
+))
+
+first_data <- fromJSON(content(first, "text", encoding = "UTF-8"))
+
+total_rows <- first_data$total
+page_size <- 100
+total_pages <- ceiling(total_rows / page_size)
+
+# loop through all pages
+all_data <- lapply(1:total_pages, get_page) |> bind_rows()
+
+# clean result
+df <- all_data %>%
+  select(Name, WAR)
+
+nrow(df)
