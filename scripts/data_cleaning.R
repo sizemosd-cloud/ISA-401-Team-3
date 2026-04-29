85602d0c0b6a9a38b48b301a4894d80fa0e94a4a
sizemosd-cloud <sizemosd@miamioh.edu>
2026-04-29 11:02:45 -0400

    Update data_cleaning.R

--- scripts/data_cleaning.R
+++ scripts/data_cleaning.R
@@ -1,7 +1,39 @@
 ##### Salaries Cleaned #####
 
-[insert]
+# Load libraries
+library(httr)
+library(jsonlite)
+library(rvest)
+library(readr)
+library(robotstxt)
 
+
+url1<- "https://www.spotrac.com/mlb/rankings/player/_/year/2025/sort/cap_total"
+paths_allowed(paths="https://www.spotrac.com/mlb/rankings/player/_/year/2025/sort/cap_total")
+
+page2 <- read_html(GET(url1, add_headers(`User-Agent` = "Mozilla/5.0")))
+
+# NOW you can scrape the elements
+player_names <- page2 |> 
+  html_elements("div.link a") |> 
+  html_text(trim = TRUE)
+
+player_salaries <- page2 |> 
+  html_elements("span.medium") |> 
+  html_text(trim = TRUE)
+
+head(player_names)
+head(player_salaries)
+
+# clean result
+
+salary_table <- data.frame(
+  Player = player_names,
+  Salary = player_salaries,
+  stringsAsFactors = FALSE
+)
+
+head(salary_table)
 
 
 ##### Position Cleaned #####
